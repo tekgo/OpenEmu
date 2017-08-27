1247745a1a885ccc1a262e3f9f7c105219e416cb
tekgo <pat@tekgo.org>
2017-08-27 12:30:02 -0700

    Add bit functions

--- OpenEmu/OpenEmuLuaHelper.m
+++ OpenEmu/OpenEmuLuaHelper.m
@@ -8,6 +8,8 @@
 
 #import "OpenEmuLuaHelper.h"
 #import <LuaCocoa/LuaCocoa.h>
+#import <LuaCocoa/lua.h>
+#import <LuaCocoa/luaconf.h>
 
 @interface OpenEmuLuaHelper ()
 
@@ -300,6 +302,185 @@ static int print(lua_State *L) {
     return 0;
 }
 
+// the following bit operations are ported from LuaBitOp 1.0.12
+// because it can handle the sign bit (bit 31) correctly.
+
+/*
+ ** Lua BitOp -- a bit operations library for Lua 5.1/5.2.
+ ** http://bitop.luajit.org/
+ **
+ ** Copyright (C) 2008-2012 Mike Pall. All rights reserved.
+ **
+ ** Permission is hereby granted, free of charge, to any person obtaining
+ ** a copy of this software and associated documentation files (the
+ ** "Software"), to deal in the Software without restriction, including
+ ** without limitation the rights to use, copy, modify, merge, publish,
+ ** distribute, sublicense, and/or sell copies of the Software, and to
+ ** permit persons to whom the Software is furnished to do so, subject to
+ ** the following conditions:
+ **
+ ** The above copyright notice and this permission notice shall be
+ ** included in all copies or substantial portions of the Software.
+ **
+ ** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ ** EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ ** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ ** IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ ** CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ ** TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ ** SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ **
+ ** [ MIT license: http://www.opensource.org/licenses/mit-license.php ]
+ */
+
+typedef int32_t SBits;
+typedef uint32_t UBits;
+
+typedef union {
+    lua_Number n;
+#ifdef LNUM_DOUBLE
+    uint64_t b;
+#else
+    UBits b;
+#endif
+} BitNum;
+
+/* Convert argument to bit type. */
+static UBits barg(lua_State *L, int idx)
+{
+    BitNum bn;
+    UBits b;
+#if LUA_VERSION_NUM < 502
+    bn.n = lua_tonumber(L, idx);
+#else
+    bn.n = luaL_checknumber(L, idx);
+#endif
+#if defined(LNUM_DOUBLE)
+    bn.n += 6755399441055744.0;  /* 2^52+2^51 */
+#ifdef SWAPPED_DOUBLE
+    b = (UBits)(bn.b >> 32);
+#else
+    b = (UBits)bn.b;
+#endif
+#elif defined(LUA_NUMBER_INT) || defined(LUA_NUMBER_LONG) || \
+defined(LUA_NUMBER_LONGLONG) || defined(LUA_NUMBER_LONG_LONG) || \
+defined(LUA_NUMBER_LLONG)
+    if (sizeof(UBits) == sizeof(lua_Number))
+        b = bn.b;
+    else
+        b = (UBits)(SBits)bn.n;
+#elif defined(LNUM_FLOAT)
+#error "A 'float' lua_Number type is incompatible with this library"
+#else
+#error "Unknown number type, check LUA_NUMBER_* in luaconf.h"
+#endif
+#if LUA_VERSION_NUM < 502
+    if (b == 0 && !lua_isnumber(L, idx)) {
+        luaL_typerror(L, idx, "number");
+    }
+#endif
+    return b;
+}
+
+/* Return bit type. */
+#define BRET(b)  lua_pushnumber(L, (lua_Number)(SBits)(b)); return 1;
+
+static int bit_tobit(lua_State *L) {
+    BRET(barg(L, 1))
+}
+
+static int bit_bnot(lua_State *L) {
+    BRET(~barg(L, 1))
+}
+
+//DEFINE_LUA_FUNCTION(bit_tobit, "x") { BRET(barg(L, 1)) }
+//DEFINE_LUA_FUNCTION(bit_bnot, "x") { BRET(~barg(L, 1)) }
+
+#define BIT_OP(func, opr) \
+static int func (lua_State *L) { int i; UBits b = barg(L, 1); \
+for (i = lua_gettop(L); i > 1; i--) b opr barg(L, i); BRET(b) }
+BIT_OP(bit_band, &=)
+BIT_OP(bit_bor, |=)
+BIT_OP(bit_bxor, ^=)
+
+#define bshl(b, n)  (b << n)
+#define bshr(b, n)  (b >> n)
+#define bsar(b, n)  ((SBits)b >> n)
+#define brol(b, n)  ((b << n) | (b >> (32-n)))
+#define bror(b, n)  ((b << (32-n)) | (b >> n))
+#define BIT_SH(func, fn) \
+static int func (lua_State *L) { \
+UBits b = barg(L, 1); UBits n = barg(L, 2) & 31; BRET(fn(b, n)) }
+BIT_SH(bit_lshift, bshl)
+BIT_SH(bit_rshift, bshr)
+BIT_SH(bit_arshift, bsar)
+BIT_SH(bit_rol, brol)
+BIT_SH(bit_ror, bror)
+
+static int bit_bswap (lua_State *L)
+{
+    UBits b = barg(L, 1);
+    b = (b >> 24) | ((b >> 8) & 0xff00) | ((b & 0xff00) << 8) | (b << 24);
+    BRET(b)
+}
+
+static int bit_tohex (lua_State *L)
+{
+    UBits b = barg(L, 1);
+    SBits n = lua_isnone(L, 2) ? 8 : (SBits)barg(L, 2);
+    const char *hexdigits = "0123456789abcdef";
+    char buf[8];
+    int i;
+    if (n < 0) { n = -n; hexdigits = "0123456789ABCDEF"; }
+    if (n > 8) n = 8;
+    for (i = (int)n; --i >= 0; ) { buf[i] = hexdigits[b & 15]; b >>= 4; }
+    lua_pushlstring(L, buf, (size_t)n);
+    return 1;
+}
+
+static const struct luaL_Reg bit_funcs[] = {
+    { "tobit",	bit_tobit },
+    { "bnot",	bit_bnot },
+    { "band",	bit_band },
+    { "bor",	bit_bor },
+    { "bxor",	bit_bxor },
+    { "lshift",	bit_lshift },
+    { "rshift",	bit_rshift },
+    { "arshift",	bit_arshift },
+    { "rol",	bit_rol },
+    { "ror",	bit_ror },
+    { "bswap",	bit_bswap },
+    { "tohex",	bit_tohex },
+    { NULL, NULL }
+};
+
+// LuaBitOp ends here
+
+static int bitshift (lua_State *L)
+{
+    int shift = luaL_checkinteger(L,2);
+    if (shift < 0) {
+        lua_pushinteger(L, -shift);
+        lua_replace(L, 2);
+        return bit_lshift(L);
+    }
+    else
+        return bit_rshift(L);
+}
+
+static int bitbit (lua_State *L)
+{
+    int rv = 0;
+    int numArgs = lua_gettop(L);
+    for(int i = 1; i <= numArgs; i++) {
+        int where = luaL_checkinteger(L,i);
+        if (where >= 0 && where < 32)
+            rv |= (1 << where);
+    }
+    lua_settop(L,0);
+    BRET(rv);
+}
+
 - (void)registerFuncs: (lua_State*)state {
     helperRef = self;
     
@@ -353,6 +534,18 @@ static int print(lua_State *L) {
     
     const luaL_reg bit_funcs[] =
     {
+        { "tobit",	bit_tobit },
+        { "bnot",	bit_bnot },
+        { "band",	bit_band },
+        { "bor",	bit_bor },
+        { "bxor",	bit_bxor },
+        { "lshift",	bit_lshift },
+        { "rshift",	bit_rshift },
+        { "arshift",	bit_arshift },
+        { "rol",	bit_rol },
+        { "ror",	bit_ror },
+        { "bswap",	bit_bswap },
+        { "tohex",	bit_tohex },
         {NULL,NULL},
     };
     
@@ -371,6 +564,13 @@ static int print(lua_State *L) {
     lua_settop(state, 0);
     
     lua_register(state, "print", print);
+    
+    // old bit operation functions
+    lua_register(state, "AND", bit_band);
+    lua_register(state, "OR", bit_bor);
+    lua_register(state, "XOR", bit_bxor);
+    lua_register(state, "SHIFT", bitshift);
+    lua_register(state, "BIT", bitbit);
 }
 
 - (void)print: (id)object {
